be85025114a87c058034382d9cde7f5652637f9b
Vítor Neto <vitordiogo2000@sapo.pt>
2022-12-09 23:10:53 +0000

    Added register example and allowed HTTP connections setting

--- jurassic-cards-unity/Assets/scripts/RegisterController.cs
+++ jurassic-cards-unity/Assets/scripts/RegisterController.cs
@@ -3,8 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using UnityEngine.Networking;
+//using System.Data.Sql;
+//using System.Data.SqlClient;
+
+using Newtonsoft.Json;
+
 public class RegisterController : MonoBehaviour
 {
+    // User
+    public class User
+    {
+        public string name;
+        public string password;
+        public string email;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     //Go to login page
     public void ChangeToLoginPage()
     {
@@ -20,7 +46,55 @@ public class RegisterController : MonoBehaviour
     //Go to Create Profile page
     public void ChangeToCreateProfilePage()
     {
-        SceneManager.LoadScene(5);
+        //SceneManager.LoadScene(5);
+        //LoadConfig();
+        StartCoroutine(UploadRegister());
+    }
+
+    IEnumerator UploadRegister()
+    {
+        var user = new User();
+        user.name = "vitor9";
+        user.email = "vitordiogo9@sapo.pt";
+        user.password = "Testing123!";
+
+        string json = JsonUtility.ToJson(user);
+
+        var req = new UnityWebRequest("http://127.0.0.1:5000/account/signup", "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        req.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        req.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        req.SetRequestHeader("Content-Type", "application/json");
+        yield return req.SendWebRequest();
+        if (req.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(req.error);
+            req.Dispose();
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+            req.Dispose();
+        }
+        
+        //WWWForm form = new WWWForm();
+        //form.AddField("name", "vitor3");
+        //form.AddField("email", "vitordiogo20002@sapo.pt");
+        //form.AddField("password", "Testing123!");
+        //using (UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:5000/account/signup", form))
+        //{
+        //    www.SetRequestHeader("Content-Type", "application/json");
+        //    yield return www.SendWebRequest();
+        //
+        //    if (www.result != UnityWebRequest.Result.Success)
+        //    {
+        //        Debug.Log(www.error);
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Form upload complete!");
+        //    }
+        //}
     }
 
     //Go back to landing page
