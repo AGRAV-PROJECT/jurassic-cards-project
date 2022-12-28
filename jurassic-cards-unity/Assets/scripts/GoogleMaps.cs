33d9a4a2952f56e5bfc3022376555b9ca86cd928
MiguelTomaz <migueltomaz.c@gmail.com>
2022-12-28 21:08:48 +0000

    logic to add fosil coordinates

--- jurassic-cards-unity/Assets/scripts/GoogleMaps.cs
+++ jurassic-cards-unity/Assets/scripts/GoogleMaps.cs
@@ -8,6 +8,11 @@ public class GoogleMaps : MonoBehaviour
     public RawImage img;
     public Text errortext;
     private string url;
+    private bool isClickedInsertFossil = false;
+    public Button buttonPlantFossil, buttonPlantFossilSubmit;
+    public GameObject insertFosilPanel;
+    public Text fossillong;
+    public Text fossillat;
 
     public double lon = 41.18089601323396;
     public double lat = -8.605617846739788;
@@ -31,6 +36,7 @@ public class GoogleMaps : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (timerIsRunning)
         {
             if (timeRemaining > 0)
@@ -47,9 +53,17 @@ public class GoogleMaps : MonoBehaviour
                 timerIsRunning = true;
             }
         }
+        buttonPlantFossilSubmit.onClick.AddListener(delegate { InsertFossilClick(); });
+        buttonPlantFossil.onClick.AddListener(delegate { OpenPanelAddFossil(); });
 
     }
 
+    public void InsertFossilClick()
+    {
+        isClickedInsertFossil = true;
+        buttonPlantFossil.gameObject.SetActive(true);
+        Debug.Log("aqui");
+    }
     IEnumerator Map()
     {
         //url = "https://www.google.com/maps/d/viewer?mid=15ypXVWq7IulnU49lDTVX1jWtCz68svU&ll=41.182212237519906%2C-8.608684771722723&z=17";
@@ -62,6 +76,19 @@ public class GoogleMaps : MonoBehaviour
 
         url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lon + "," + lat + "&zoom=" + zoom + "&size=" + width + "x" + height + "&scale=" + scale + "&maptype=roadmap&style=element:geometry%7Ccolor:0x1d2c4d&style=element:labels.text.fill%7Ccolor:0x8ec3b9&style=element:labels.text.stroke%7Ccolor:0x1a3646&style=feature:administrative.country%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0x64779e&style=feature:administrative.province%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:landscape.man_made%7Celement:geometry.stroke%7Ccolor:0x334e87&style=feature:landscape.natural%7Celement:geometry%7Ccolor:0x023e58&style=feature:poi%7Celement:geometry%7Ccolor:0x283d6a&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x6f9ba5&style=feature:poi%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:poi.park%7Celement:geometry.fill%7Ccolor:0x023e58&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x3C7680&style=feature:road%7Celement:geometry%7Ccolor:0x304a7d&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x98a5be&style=feature:road%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:road.highway%7Celement:geometry%7Ccolor:0x2c6675&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0x255763&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0xb0d5ce&style=feature:road.highway%7Celement:labels.text.stroke%7Ccolor:0x023e58&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x98a5be&style=feature:transit%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:transit.line%7Celement:geometry.fill%7Ccolor:0x283d6a&style=feature:transit.station%7Celement:geometry%7Ccolor:0x3a4762&style=feature:water%7Celement:geometry%7Ccolor:0x0e1626&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x4e6d70&size=480x360&key=AIzaSyA1-laJALAbLoiSNXQ7ZVgMS1PpJ297HJw&markers=color:green%7Clabel:D%7C41.18089601323396,-8.605617846739788&key=AIzaSyA1-laJALAbLoiSNXQ7ZVgMS1PpJ297HJw";
 
+
+        var index = url.IndexOf("&zoom=");
+        if(isClickedInsertFossil)
+        {
+            if (index >= 0)
+            {
+                url = url.Insert(index, "&markers=color:blue%7Clabel:F%7C" + fossillong.text + "," + fossillat.text + "&key=AIzaSyA1-laJALAbLoiSNXQ7ZVgMS1PpJ297HJw");
+                Debug.Log("url3: " + url);
+                isClickedInsertFossil = false;
+                insertFosilPanel.SetActive(false);
+            }
+        }
+
         //url = "https://maps.googleapis.com/maps/api/staticmap?center=41.18089601323396,-8.605617846739788&zoom=10&size=500x500&scale=4&maptype=roadmap&style=element:geometry%7Ccolor:0x1d2c4d&style=element:labels.text.fill%7Ccolor:0x8ec3b9&style=element:labels.text.stroke%7Ccolor:0x1a3646&style=feature:administrative.country%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0x64779e&style=feature:administrative.province%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:landscape.man_made%7Celement:geometry.stroke%7Ccolor:0x334e87&style=feature:landscape.natural%7Celement:geometry%7Ccolor:0x023e58&style=feature:poi%7Celement:geometry%7Ccolor:0x283d6a&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x6f9ba5&style=feature:poi%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:poi.park%7Celement:geometry.fill%7Ccolor:0x023e58&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x3C7680&style=feature:road%7Celement:geometry%7Ccolor:0x304a7d&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x98a5be&style=feature:road%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:road.highway%7Celement:geometry%7Ccolor:0x2c6675&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0x255763&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0xb0d5ce&style=feature:road.highway%7Celement:labels.text.stroke%7Ccolor:0x023e58&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x98a5be&style=feature:transit%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:transit.line%7Celement:geometry.fill%7Ccolor:0x283d6a&style=feature:transit.station%7Celement:geometry%7Ccolor:0x3a4762&style=feature:water%7Celement:geometry%7Ccolor:0x0e1626&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x4e6d70&size=480x360&key=AIzaSyA1-laJALAbLoiSNXQ7ZVgMS1PpJ297HJw";
 
         //url = "https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyA1-laJALAbLoiSNXQ7ZVgMS1PpJ297HJw&center=41.17003932872469,-8.607315897900389&zoom=13&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0x1d2c4d&style=element:labels.text.fill%7Ccolor:0x8ec3b9&style=element:labels.text.stroke%7Ccolor:0x1a3646&style=feature:administrative.country%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0x64779e&style=feature:administrative.province%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:landscape.man_made%7Celement:geometry.stroke%7Ccolor:0x334e87&style=feature:landscape.natural%7Celement:geometry%7Ccolor:0x023e58&style=feature:poi%7Celement:geometry%7Ccolor:0x283d6a&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x6f9ba5&style=feature:poi%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:poi.park%7Celement:geometry.fill%7Ccolor:0x023e58&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x3C7680&style=feature:road%7Celement:geometry%7Ccolor:0x304a7d&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x98a5be&style=feature:road%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:road.highway%7Celement:geometry%7Ccolor:0x2c6675&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0x255763&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0xb0d5ce&style=feature:road.highway%7Celement:labels.text.stroke%7Ccolor:0x023e58&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x98a5be&style=feature:transit%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:transit.line%7Celement:geometry.fill%7Ccolor:0x283d6a&style=feature:transit.station%7Celement:geometry%7Ccolor:0x3a4762&style=feature:water%7Celement:geometry%7Ccolor:0x0e1626&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x4e6d70&size=480x360";
@@ -73,6 +100,11 @@ public class GoogleMaps : MonoBehaviour
         img.SetNativeSize();
     }
 
+    public void OpenPanelAddFossil()
+    {
+        insertFosilPanel.SetActive(true);
+        buttonPlantFossil.gameObject.SetActive(false);
+    }
     IEnumerator GPSUserLocation()
     {
         if(!Input.location.isEnabledByUser)
